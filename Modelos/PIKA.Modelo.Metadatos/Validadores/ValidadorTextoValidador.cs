c00c3a218c6cca6e70a9176f28a03e6758c97ca7
Maria Luisa Vega <mvega@arguz.com>
2020-04-10 19:00:26 -0500

    Cambios ServiciosMetdatos

---
+++ Modelos/PIKA.Modelo.Metadatos/Validadores/ValidadorTextoValidador.cs
@@ -0,0 +1,40 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.Localization;
+using RepositorioEntidades;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PIKA.Modelo.Metadatos.Validadores
+{
+    public class ValidadorTextoValidador : AbstractValidator<ValidadorTexto>
+    {
+        public ValidadorTextoValidador(IStringLocalizer<ValidadorTexto> localizer)
+        {
+            RuleFor(x => x.PropiedadId)
+                .NotNull().WithMessage(x => localizer["El id propiedad es obligatorio"])
+                .NotEmpty().WithMessage(x => localizer["El id propiedad es obligatorio"])
+                .MinimumLength(1).WithMessage(x => localizer["El id propiedad debe tener entre {0} y {1} caracteres", 1, LongitudDatos.Nombre])
+                .MaximumLength(LongitudDatos.Nombre).WithMessage(x => localizer["El id propiedad debe tener entre {0} y {1} caracteres", 1, LongitudDatos.Nombre]);
+
+            RuleFor(x => x.longmin)
+          .NotNull().WithMessage(x => localizer["La longitud minima es obligatoria"])
+          .NotEmpty().WithMessage(x => localizer["La longitud minima es obligatorio"]);
+            RuleFor(x => x.longmax)
+                    .NotNull().WithMessage(x => localizer["La longuitud màxima es obligatoria"])
+                    .NotEmpty().WithMessage(x => localizer["La longuitud màxima es obligatorio"]);
+       
+            RuleFor(x => x.valordefaulr)
+                          .NotNull().WithMessage(x => localizer["El valor default es obligatorio"])
+                          .NotEmpty().WithMessage(x => localizer["El valor default es obligatorio"])
+                          .MinimumLength(1).WithMessage(x => localizer["El valor default debe tener entre {0} y {1} caracteres", 1, LongitudDatos.Nombre])
+                          .MaximumLength(LongitudDatos.Nombre).WithMessage(x => localizer["El valor default debe tener entre {0} y {1} caracteres", 1, LongitudDatos.Nombre]);
+            RuleFor(x => x.regexp)
+                   .NotNull().WithMessage(x => localizer["El regexp es obligatorio"])
+                   .NotEmpty().WithMessage(x => localizer["El regexp es obligatorio"])
+                   .MinimumLength(1).WithMessage(x => localizer["El regexp debe tener entre {0} y {1} caracteres", 1, LongitudDatos.Nombre])
+                   .MaximumLength(LongitudDatos.Nombre).WithMessage(x => localizer["El regexp debe tener entre {0} y {1} caracteres", 1, LongitudDatos.Nombre]);
+
+        }
+    }
+}
