41df8c1e2dc9b178b4ce6b52d4249a896e4a91db
Maria Luisa Vega <mvega@arguz.com>
2020-04-15 12:13:15 -0500

    Cambios Actualización DbContext

---
+++ Servicios/Metadatos/PIKA.Servicio.Metadatos/Servicios/ContextoServicioMetadatos.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.Logging;
+using PIKA.Infraestructura.Comun;
+using PIKA.Servicio.Metadatos.Data;
+using RepositorioEntidades;
+
+namespace PIKA.Servicio.Metadatos.Servicios
+{
+  public  class ContextoServicioMetadatos
+    {
+        private IProveedorOpcionesContexto<DbContextMetadatos> proveedorOpciones;
+        protected IServicioCache cache;
+        protected ILogger logger;
+
+        protected DbContextMetadatos contexto;
+        public ContextoServicioMetadatos(
+            IProveedorOpcionesContexto<DbContextMetadatos> proveedorOpciones,
+            ILogger Logger,
+            IServicioCache servicioCache)
+        {
+            DbContextMetadatosFactory cf = new DbContextMetadatosFactory(proveedorOpciones);
+            this.contexto = cf.Crear();
+            this.cache = servicioCache;
+            this.logger = Logger;
+        }
+    }
+}
