110bf732fcc2db546b6c43a18d4812c509dc5c13
Desarrollo <desarrollo@arguz.com>
2020-05-19 13:29:45 -0500

    Iclusion proyecto metadatos + elasticseach

---
+++ Servicios/Metadatos/PIKA.Servicio.Metadatos.ElasticSearch/ClienteMetadatos.cs
@@ -0,0 +1,25 @@
+﻿using Nest;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PIKA.Servicio.Metadatos.ElasticSearch
+{
+    public class ClienteMetadatos
+    {
+        public void Indexar()
+        {
+            string url = "http://localhost:9200/";
+            string index = "datos";
+
+            var settings = new ConnectionSettings(new Uri(url))
+          .DefaultIndex(index);
+
+            var client = new ElasticClient(settings);
+
+
+
+        }
+
+    }
+}
