64af08cae646527c6517b740b5e4bf00f3c0df9a
Maria Luisa Vega <mvega@arguz.com>
2020-07-30 12:13:09 -0500

    Postman

--- Modelos/PIKA.Modelo.GestorDocumental/Validadores/ArchivoValidador.cs
+++ Modelos/PIKA.Modelo.GestorDocumental/Validadores/ArchivoValidador.cs
@@ -10,23 +10,10 @@ namespace PIKA.Modelo.GestorDocumental.Validadores
     public class ArchivoValidador : AbstractValidator<Archivo>
     {
         public ArchivoValidador(IStringLocalizer<Archivo> localizer) {
-            RuleFor(x => x.Nombre)
-                .NotNull().WithMessage(x => localizer["El nombre es obligatorio"])
-                .NotEmpty().WithMessage(x => localizer["El nombre es obligatorio"])
-                .MinimumLength(1).WithMessage(x => localizer["El nombre debe tener entre {0} y {1} caracteres", 1, LongitudDatos.Nombre])
-                .MaximumLength(LongitudDatos.Nombre).WithMessage(x => localizer["El nombre debe tener entre {0} y {1} caracteres", 1, LongitudDatos.Nombre]);
-
-            RuleFor(x => x.TipoArchivoId)
-                .NotNull().WithMessage(x => localizer["El tipo de archivo es obligatorio"])
-                .NotEmpty().WithMessage(x => localizer["El tipo de archivo es obligatorio"]);
-
-            RuleFor(x => x.TipoOrigenId)
-                .NotNull().WithMessage(x => localizer["El tipo de origen es obligatorio"])
-                .NotEmpty().WithMessage(x => localizer["El tipo de origen es obligatorio"]);
-
-            RuleFor(x => x.OrigenId)
-                .NotNull().WithMessage(x => localizer["El origen es obligatorio"])
-                .NotEmpty().WithMessage(x => localizer["El origen es obligatorio"]);
+            RuleFor(x => x.Nombre).NotEmpty().MinimumLength(1).MaximumLength(LongitudDatos.Nombre);
+            RuleFor(x => x.TipoArchivoId).NotEmpty().MinimumLength(1).MaximumLength(LongitudDatos.GUID);
+            RuleFor(x => x.TipoOrigenId).NotEmpty().MinimumLength(1).MaximumLength(LongitudDatos.GUID);
+            RuleFor(x => x.OrigenId).NotEmpty().MinimumLength(1).MaximumLength(LongitudDatos.GUID);
 
         }
     }
