6b4fd65d26e0a1fdec6ee953af388e08718b3d31
Maria Luisa Vega <mvega@arguz.com>
2020-08-11 14:54:40 -0500

    merge

---
+++ Servicios/GestionDocumental/PIKA.Servicio.GestionDocumental/Data/Exportar_Importar/IOCuadroClasificacion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PIKA.Servicio.GestionDocumental.Data.Exportar_Importar
+{
+  public  class IOCuadroClasificacion
+    {
+        public void ExportarCuadroCalsificacionExcel(string id) 
+        { 
+        }
+
+        public int NumeroCulumna { get; set; }
+        public int NumeroRenglon { get; set; }
+        public string PosicionColumna { get; set; }
+        public string ValorRenglon { get; set; }
+        public int Region { get; set; }
+
+    }
+}
