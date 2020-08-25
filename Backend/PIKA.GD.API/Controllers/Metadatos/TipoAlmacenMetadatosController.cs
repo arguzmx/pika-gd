a7210f8240744d42e84b85bec2baa064049265c6
Maria Luisa Vega <mvega@arguz.com>
2020-08-25 10:16:08 -0500

    revisin oswaldo

--- Backend/PIKA.GD.API/Controllers/Metadatos/TipoAlmacenMetadatosController.cs
+++ Backend/PIKA.GD.API/Controllers/Metadatos/TipoAlmacenMetadatosController.cs
@@ -43,10 +43,16 @@ namespace PIKA.GD.API.Controllers.Metadatos
         [TypeFilter(typeof(AsyncACLActionFilter))]
         public async Task<ActionResult<MetadataInfo>> GetMetadata([FromQuery] Consulta query = null)
         {
+            Console.WriteLine("\n Ingreso.... \n");
             return Ok(await metadataProvider.Obtener().ConfigureAwait(false));
         }
 
 
+        /// <summary>
+        /// Añade una nueva entidad del tipo Tipo Almacen Metadatos
+        /// </summary>
+        /// <param name="entidad"></param>
+        /// <returns></returns>
 
         /// <summary>
         /// Añade una nueva entidad de Tipo Almacen Metadatos
@@ -88,6 +94,11 @@ namespace PIKA.GD.API.Controllers.Metadatos
 
         }
 
+        /// <summary>
+        /// Devulve un alista de Tipo Almacen Metadatos asociadas al objeto del tipo especificado
+        /// </summary>
+        /// <param name="query">Consulta para la paginación y búsqueda</param>
+        /// <returns></returns>
 
         /// <summary>
         /// Devulve una lista de Tipo Almacen Metadatos asociadas al objeto del tipo especificado
@@ -110,6 +121,10 @@ namespace PIKA.GD.API.Controllers.Metadatos
         }
 
 
+        //----------------------------------------------------------------------------------------------------------
+        //----------------------------------------------------------------------------------------------------------
+        //----------------------------------------------------------------------------------------------------------
+        //----------------------------------------------------------------------------------------------------------
 
         /// <summary>
         /// Obtiene un país en base al Id único
@@ -132,6 +147,11 @@ namespace PIKA.GD.API.Controllers.Metadatos
 
 
 
+        /// <summary>
+        /// Elimina de manera permanente un Tipo Almacen Metadatos en base al arreglo de identificadores recibidos
+        /// </summary>
+        /// <param name="ids">Arreglo de identificadores string</param>
+        /// <returns></returns>
 
         /// <summary>
         /// Elimina de manera permanente un Tipo Almacen Metadatos en base al arreglo de identificadores recibidos
