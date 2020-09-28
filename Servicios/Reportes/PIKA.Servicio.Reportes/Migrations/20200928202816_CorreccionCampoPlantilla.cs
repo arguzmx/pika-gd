add3b45d6367c909424e92d0a83035b083a5a98d
mvega-arguz <62155277+mvega-arguz@users.noreply.github.com>
2020-09-28 15:32:40 -0500

    Modificacion DBConfig LongText

---
+++ Servicios/Reportes/PIKA.Servicio.Reportes/Migrations/20200928202816_CorreccionCampoPlantilla.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PIKA.Servicio.Reportes.Migrations
+{
+    public partial class CorreccionCampoPlantilla : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Plantilla",
+                table: "repo$reporteentidad",
+                maxLength: 429496729,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(512) CHARACTER SET utf8mb4",
+                oldMaxLength: 512);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Plantilla",
+                table: "repo$reporteentidad",
+                type: "varchar(512) CHARACTER SET utf8mb4",
+                maxLength: 512,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 429496729);
+        }
+    }
+}
