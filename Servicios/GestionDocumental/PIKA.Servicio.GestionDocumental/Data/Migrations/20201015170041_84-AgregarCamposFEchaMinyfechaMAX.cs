53da3bcf7b44b226cb2cf21f9ed1939a118fcac2
mvega-arguz <62155277+mvega-arguz@users.noreply.github.com>
2020-10-15 13:58:47 -0500

    Ticket 84-FEchaMinYMax

---
+++ Servicios/GestionDocumental/PIKA.Servicio.GestionDocumental/Data/Migrations/20201015170041_84-AgregarCamposFEchaMinyfechaMAX.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PIKA.Servicio.GestionDocumental.Data.Migrations
+{
+    public partial class _84AgregarCamposFEchaMinyfechaMAX : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "FechaMaxCierre",
+                table: "gd$estadisticaclasificacionacervo",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "FechaMinApertura",
+                table: "gd$estadisticaclasificacionacervo",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FechaMaxCierre",
+                table: "gd$estadisticaclasificacionacervo");
+
+            migrationBuilder.DropColumn(
+                name: "FechaMinApertura",
+                table: "gd$estadisticaclasificacionacervo");
+        }
+    }
+}
