d49d7031d4d6fe2b298a2dd5cd67d4936f10f445
Desarrollo Arguz <desarrollo@arguz.com>
2021-05-19 09:53:43 -0500

    Migraciones gestion documental

---
+++ Servicios/GestionDocumental/PIKA.Servicio.GestionDocumental/Data/Migrations/20210517184144_VolumenArchivo.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PIKA.Servicio.GestionDocumental.Data.Migrations
+{
+    public partial class VolumenArchivo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "VolumenDefaultId",
+                table: "gd$archivo",
+                maxLength: 128,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "VolumenDefaultId",
+                table: "gd$archivo");
+        }
+    }
+}
