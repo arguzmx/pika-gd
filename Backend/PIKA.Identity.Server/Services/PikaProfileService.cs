0595e900d8e73daa571450845897aff337dd43a3
Desarrollo Arguz <desarrollo@arguz.com>
2022-07-11 20:56:09 -0500

    V1.4.11

--- Backend/PIKA.Identity.Server/Services/PikaProfileService.cs
+++ Backend/PIKA.Identity.Server/Services/PikaProfileService.cs
@@ -14,8 +14,6 @@ namespace PIKA.Identity.Server.Services
     using IdentityServer4;
     using PIKA.Modelo.Seguridad;
     using PIKA.Servicio.Usuarios;
-    using System.IO;
-    using System.Text.Json;
 
     public class PikaProfileService : IProfileService
     {
@@ -33,58 +31,42 @@ namespace PIKA.Identity.Server.Services
 
         public async Task GetProfileDataAsync(ProfileDataRequestContext context)
         {
-            try
+            var sub = context.Subject.GetSubjectId();
+            var user = await _userManager.FindByIdAsync(sub);
+            var principal = await _claimsFactory.CreateAsync(user);
+
+            var claims = principal.Claims.ToList();
+            claims = claims.Where(claim => context.RequestedClaimTypes.Contains(claim.Type)).ToList();
+            claims.Add(new Claim(JwtClaimTypes.GivenName, user.UserName));
+            
+
+            List<string> roles = await perfil.ObtieneRoles(sub);
+            roles.ForEach(r => {
+                claims.Add(new Claim(JwtClaimTypes.Role, r));
+            });
+
+            if(await perfil.EsAdmin(sub,""))
             {
-                File.AppendAllText("log.txt", $"{1}\r\n");
-                var sub = context.Subject.GetSubjectId();
-                var user = await _userManager.FindByIdAsync(sub);
-                var principal = await _claimsFactory.CreateAsync(user);
-
-                File.AppendAllText("log.txt", $"{2}\r\n");
-
-                var claims = principal.Claims.ToList();
-                claims = claims.Where(claim => context.RequestedClaimTypes.Contains(claim.Type)).ToList();
-                claims.Add(new Claim(JwtClaimTypes.GivenName, user.UserName));
-
-                File.AppendAllText("log.txt", $"{3}\r\n");
-                List<string> roles = await perfil.ObtieneRoles(sub);
-                roles.ForEach(r => {
-                    File.AppendAllText("log.txt", $"{r}\r\n");
-                    claims.Add(new Claim(JwtClaimTypes.Role, r));
-                });
-
-                if (await perfil.EsAdmin(sub, ""))
-                {
-                    claims.Add(new Claim(JwtClaimTypes.Role, "adminglobal"));
-                }
-
-                var dominios = await perfil.Dominios(sub);
-                dominios.ForEach(d =>
-                {
-                    File.AppendAllText("log.txt", $"{JsonSerializer.Serialize(d , new JsonSerializerOptions() { WriteIndented= true})}\r\n");
-                    d.UnidadesOrganizacionales.ForEach(u =>
-                    {
-                        var value = u.DominioId + ':' + u.Id + (d.EsAdmin ? ":admin" : ":user");
-                        File.AppendAllText("log.txt", $"{value}\r\n");
-                        claims.Add(new Claim(JwtClaimTypes.Role, value));
-
-                    });
-
-                });
-
-                //    claims.Add(new Claim(JwtClaimTypes.Scope, "dataEventRecords"));
-
-                claims.Add(new Claim(IdentityServerConstants.StandardScopes.Email, user.Email ?? ""));
-
-                context.IssuedClaims = claims;
-            }
-            catch (Exception ex)
-            {
-                File.AppendAllText("log.txt" , $"{ex}\r\n");
-                throw;
+                claims.Add(new Claim(JwtClaimTypes.Role, "adminglobal"));
             }
 
-           
+            var dominios = await perfil.Dominios(sub);
+            dominios.ForEach(d =>
+            {
+                d.UnidadesOrganizacionales.ForEach(u =>
+                {
+                    var value = u.DominioId + ':' + u.Id + (d.EsAdmin ? ":admin" : ":user");
+                    claims.Add(new Claim(JwtClaimTypes.Role, value));
+
+                });
+                
+            });
+
+            //    claims.Add(new Claim(JwtClaimTypes.Scope, "dataEventRecords"));
+
+            claims.Add(new Claim(IdentityServerConstants.StandardScopes.Email, user.Email ?? "" ));
+
+            context.IssuedClaims = claims;
         }
 
         public async Task IsActiveAsync(IsActiveContext context)
